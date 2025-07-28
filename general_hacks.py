b5ddb727d07ce0eeb8827ad33e9b4e9c004cfe39
pentafeeder <kevinzeawesome@gmail.com>
2025-07-28 13:38:28 +1000

    build/dependency:added files for pyMeow module.
    feat: added 2 extra files for wallhacks and aimbot and also tweaked original hack file. Removed trigger bot idea and added remove recoil

--- general_hacks.py
+++ general_hacks.py
@@ -1,37 +1,35 @@
-import math
 import sys
-import tkinter as tk
-import ctypes
 import pymem 
 from pymem.ptypes import RemotePointer 
-import array
-import vectormath as vmath
-#import pyMeow as pm
+import math
+from pynput import keyboard
+from pynput.keyboard import Listener as KeyListener
+
 
 # get our process
 process_name = 'ac_client.exe'
 process = pymem.Pymem(process_name)
+base_address = process.base_address
 
 # setup
 alloc_mem_addr = process.allocate(0x12)
 
+
+print(base_address)
+
 class address:
-    FOV = process.base_address + 0x18A7CC
-    player_ent_ptr = process.base_address + 0x18AC00 # should be 58AC00
+    FOV = base_address + 0x18A7CC
+    player_ent_ptr = base_address + 0x18AC00 # should be 58AC00
     player_ent_addr = RemotePointer(process.process_handle, player_ent_ptr).value
     num_players = player_ent_ptr  + 0xC
     local_ent_list_ptr = player_ent_ptr + 0x04
-
-    # menu FOV = process.base_address + 0x185884 -- need to resolve through multi-level pointer
+    traceline_func = 0x509010 #placeholder
+    print(player_ent_ptr, player_ent_addr)
 
 class offsets:
     name = 0x205
-    health = 0xEC
-    FOV = [0x14, 0x1C, 0x20]
-    
+    health = 0xEC   
     team = 0x30C
-
-    team_from_health = health - team #224
 
     ar_ammo = 0x140
     carbine_ammo = 0x130
@@ -53,13 +51,22 @@ class offsets:
     head_pos_z = 0xC
     head_pos_ns = 0x8 
 
+    auto_fire = 0x204
+    current_weapon_ent = 0x368
+    current_weapon_id = 0x4 # from current weapon ent
+    
     ammo_dec = 0xC73EF
-    #health sub = 0x1C223, 3 bytes
+    health_sub = 0x1C223
+    recoil = 0xC2EC3 
+
+
+def removeRecoil():
+    process.write_bytes(base_address + offsets.recoil, int('90'*5, 16).to_bytes(5, 'big'), 5) # writes 'nop' instruction to existing dec / store instruction
+    print('recoil removed')
 
 def infAmmo():
-    process.write_bytes(process.base_address + offsets.ammo_dec, int('9090', 16).to_bytes(2, 'big'), 2) # writes 'nop' instruction to existing dec / store instruction
+    process.write_bytes(base_address + offsets.ammo_dec, int('9090', 16).to_bytes(2, 'big'), 2)
     print('you now have infinite ammo')
-    pass
 
 def changeValue(base_address, offset, increment):
     try:
@@ -78,66 +85,12 @@ def increaseHealth():
 
 def addGrenade():
     changeValue(address.player_ent_addr, offsets.grenade, 1)
-    print('added grenade to inventory')
-
-# for values stored in multi-level pointers like FOV
-# obsolete, since I found the real FOV address
-def traversePointers(base_address, offsets):
-    address = RemotePointer(process.process_handle, base_address).value
-    for offset in offsets:
-        try:
-            dereference = RemotePointer(process.process_handle, address + offset).value
-            address = dereference
-        except Exception as e:
-            print('end of pointer')
-            return address
-    # by end, address is a primary value
-    return address
-
-
-def trackOthers():
-    player_addr = address.player_ent_addr
-    ent_list_addr = RemotePointer(process.process_handle, address.local_ent_list_ptr).value
-    enemy_addr = RemotePointer(process.process_handle, ent_list_addr).value
-    if (enemy_addr == 0):
-        enemy_addr = RemotePointer(process.process_handle, ent_list_addr + 0x4).value
-
-    enemy_coords = [0, 0, 0] #x, y, z, (east/west, south/north, up/down)
-    player_coords = [0, 0, 0]
-    
-    
-    enemy_coords[0] = process.read_float(enemy_addr + offsets.head_pos_ns)
-    enemy_coords[1] = process.read_float(enemy_addr + offsets.head_pos_ew)
-    enemy_coords[2] = process.read_float(enemy_addr + offsets.head_pos_z)
-    
-    player_coords[0] = process.read_float(player_addr + offsets.head_pos_ns)
-    player_coords[1] = process.read_float(player_addr + offsets.head_pos_ew)
-    player_coords[2] = process.read_float(player_addr + offsets.head_pos_z)
-    
-    # vectors between player and enemy
-    vx = player_coords[0] - enemy_coords[0]
-    vy = player_coords[1] - enemy_coords[1]
-    vz = player_coords[2] - enemy_coords[2]
-
-    distance = math.sqrt(vx**2 + vy**2 + vz**2)
-
-    print('The distance between you and an enemy is:', distance)
-    if (distance > 30):
-        print('you are far from them')
-    elif (distance > 15):
-        print('you are quite near them')
-    else:
-        print('very close!!!')
-    
-    
-
-
-    
+    print('added grenade to inventory')        
 
 def changeFOV(value):    
     try:
-        print(hex(address.FOV), float(value))
         process.write_float(address.FOV, float(value))
+        print('fov changed')
     except Exception as e:
         print('please input a positive number')
 
@@ -151,7 +104,11 @@ def makeInvulnerable(option):
         print('invalid option, defaulting to team')
     
     # change team for invulnerability
-    process.write_bytes(address.player_ent_addr + offsets.team, int(f'{team_addr.value:0{2}x}', 16).to_bytes(2, 'big'), 2) # change team to match invulnerability 
+    print(int(f'{team_addr.value:0{2}x}', 16))
+    print(int(f'{team_addr.value:0{2}x}', 16).to_bytes(1, 'big'))
+    print(int(f'{team_addr.value:0{2}x}', 16).to_bytes(1, 'little'))
+
+    process.write_bytes(address.player_ent_addr + offsets.team, int(f'{team_addr.value:0{2}x}', 16).to_bytes(1, 'big'), 1) # change team to match invulnerability 
 
     # inject code into malloced memory    
     process.write_bytes(alloc_mem_addr, int('83BB24020000' + f'{team_addr.value:0{2}x}', 16).to_bytes(7, 'big'), 7) # cmp dword ptr [ebx+00000224],00 
@@ -160,7 +117,7 @@ def makeInvulnerable(option):
     process.write_bytes(alloc_mem_addr + 0x10, int('8BC6', 16).to_bytes(2, 'big'), 2) # mov eax,esi
     
     # getting byte difference between mem addr of old instructions and mem addr of injected instructions
-    opcode_bytes = str(alloc_mem_addr - process.base_address - 0x1C223 - 0x5)
+    opcode_bytes = str(alloc_mem_addr - base_address - offsets.health_sub - 0x5)
     num_instruction_bytes = 0x12 # 18 bytes
     return_opcode = hex((~(int(bytes(opcode_bytes, 'utf-8')) + num_instruction_bytes + 0x5) & (2**32 - 1)) + 1).split('x')[1]
     opcode_bytes = hex(int(opcode_bytes)).split('x')[1]
@@ -170,7 +127,7 @@ def makeInvulnerable(option):
     return_opcode = 'E9' + swapEndianness(return_opcode)
     
     #jump to injected code
-    process.write_bytes(process.base_address + 0x1C223, int(opcode_bytes, 16).to_bytes(5, 'big'), 5) 
+    process.write_bytes(base_address + offsets.health_sub, int(opcode_bytes, 16).to_bytes(5, 'big'), 5) 
     
     #jump back to previous code and continue
     process.write_bytes(alloc_mem_addr + 0x12, int(return_opcode, 16).to_bytes(5, 'big'), 5) # jmp ac_client.exe+1C228
@@ -188,39 +145,66 @@ def swapEndianness(string):
 
     return ''.join(arr)
 
-def main():
-    while True:
-        print("Type the number corresponding to what hack you want to have:")
-        print("1. infinite ammo")
-        print("2. increased health")
-        print("3. get distance")
-        print('4. add grenade to inventory')
-        print('5. become invulnerable')
-        print('6. change FOV')
-        hack = input("").strip()
-        
-        match hack:
-            case '1':
+def clearHacks():
+    process.write_bytes(base_address + offsets.health_sub, int('2973048bc6', 16).to_bytes(5, 'big'), 5) 
+    process.write_bytes(base_address + offsets.ammo_dec, int('FF08', 16).to_bytes(2, 'big'), 2)
+    process.write_bytes(base_address + offsets.recoil, int('F30F115638', 16).to_bytes(5, 'big'), 5) 
+    print('hacks cleared')
+
+
+def on_press(key):
+    try: 
+        match key:
+            case keyboard.Key.f1:
                 print('getting infinite ammo')
                 infAmmo()
-            case '2':
+            case keyboard.Key.f2:
                 print('getting increased health / invulnerability')
                 increaseHealth()
-            case '3':
-                print('tracking distance')
-                trackOthers()
-            case '4':
+            case keyboard.Key.f3:
+                print('removing recoil')
+                removeRecoil()
+            case keyboard.Key.f4:
                 print('getting grenade')
                 addGrenade()
-            case '5':
+            case keyboard.Key.f5:
                 option = input('type "team" or "solo" to include for invincibility: ')
                 makeInvulnerable(option)
-            case '6':
+            case keyboard.Key.f6:
                 value = input('type a positive number between 0 and 170 to set the FOV: ')
                 changeFOV(value)
+            case keyboard.Key.esc:
+                print('exiting')
+                return False
+            case keyboard.Key.enter:
+                printInstructions()
+            case keyboard.Key.delete:
+                clearHacks()
             case _:
-                print('invalid hack')
-        
+                pass
+    except: 
+        pass 
+
+def printInstructions():
+    print("Type corresponding key to choose what you want to have/do:")
+    print("f1. infinite ammo")
+    print("f2. increased health")
+    print("f3. remove recoil")
+    print('f4. add grenade to inventory')
+    print('f5. become invulnerable')
+    print('f6. change FOV')
+    print('esc: exit program')
+    print('del: clear hacks')
+    print('enter: reprint instructions')
+
+
+def main():
+    printInstructions() 
+
+    listener = KeyListener(on_press=on_press)
+    listener.start()
+    listener.join()
+
 
 if __name__ == "__main__":
     print("starting hack")
@@ -229,4 +213,5 @@ if __name__ == "__main__":
     finally:
         #free malloced memory and write back old instructions (if changed)
         process.free(alloc_mem_addr) 
-        process.write_bytes(process.base_address + 0x1C223, int('2973048bc6', 16).to_bytes(5, 'big'), 5) 
+        clearHacks()
+        
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+   name='pyMeow',
+   version='1.73.42',
+   description='Python Library for external Game Hacking',
+   author='qb',
+   packages=['pyMeow'],
+   include_package_data=True,
+   url='https://github.com/qb-0/PyMeow',
+   license='MIT'
+)
